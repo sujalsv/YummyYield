4c32e9fd780f2a219c66c74b782f9bd28b09a0ba
sujalsv <111581568+sujalsv@users.noreply.github.com>
2024-01-12 00:57:23 +0530

    epi 3

--- App.js
+++ App.js
@@ -1,54 +1,32 @@
-// const heading = React.createElement("h1", {id:"heading"}, "Hello World from React");
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-// console.log(heading);   gives react object
+const heading = React.createElement("h1", { id: "heading" }, "Namaste React ");
+console.log(heading);
 
-// const root = ReactDOM.createRoot(document.getElementById("root"));
+const jsxHeading = <h1 id="heading">Namaste React using jsx(element)</h1>;
+console.log(jsxHeading);
 
-// root.render(heading); 
-// responsible for taking h1 tag and putting it over heading and put whole inside root
-
-
-
-/*
- * <div id="parent">
- *     <div id="child">
- *      <h1>i m h1</h1>
- *     </div>
- * <div>
- */
-
-// nested elements using html 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(jsxHeading);
+//rendering element in rreact
 
 
-// const parent= React.createElement("div",{id:"parent"},
-// React.createElement("div",{id:"child"},
-// React.createElement("h1",{},"i m h1")));
-
-// console.log(parent);
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(parent); 
-
-// nested elements using react 
-
-//  React.createElement() does not create an elemnt instead forms an object that html (browser understands)
-// render converts into react component and puts it into DOM
-// to pass the sibling instead of children one need to wrap childrens in an array of parent
-
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "i m h1"),
-    React.createElement("h2", {}, "i m h2"),
-  ])
+// const HeadingComponent=()=>{
+//     return <h1 className="heading">namaste react functional component</h1>
+// }
+const HeadingComponent = () => (
+  <div>
+    {jsxHeading}
+    <h1 className="heading">namaste react (functional component)</h1>
+  </div>
 );
 
-console.log(parent);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent); 
+root.render(<HeadingComponent/>);
+//rendering element in jsx
 
-// ignore errors in console will solve later on
-// the above code keeps on getting messy if we includes more and more children , thats where JSX enters to make more TAGS in simple manner
-// if div tag is already there in render before root.render then root.render will replace things because browser reads html first then goes to script line and reads react and root.render in app.js replaces
-// everything will be rendered inside root, while things which are below and after the root in html file will remain the same
-// react id a library bcz it works only on one small elemnt or tag like render and it does not comes with full advantages like full functional framework
+
+// {Title()}
+// <Title/>
+// <Title></Title>
+// all 3 same things
